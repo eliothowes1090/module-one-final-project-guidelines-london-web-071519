65131925b80c0c69e558d57901f658734e8e7833
eliothowes1090 <ejhowes@gmail.com>
2019-07-30 21:57:24 +0100

    Refactored methods in cli_user_methods. Refactored cli_runner to remove if/else statements.

--- lib/cli/cli_runner.rb
+++ lib/cli/cli_runner.rb
@@ -5,35 +5,10 @@ require_relative 'cli_welcome_and_login.rb'
 def cli_runner
     welcome_message
 
-    inputted_login_type = login_type
-    if inputted_login_type == "User Login"
-       current_user =  user_login
-    elsif inputted_login_type == "Create new User Account"
-        create_new_user_account
-    elsif inputted_login_type == "Concert Login"
-        organisation_login
-    elsif inputted_login_type == "Create new Concert Account"
-        create_new_organisation_account
-    end
+    current_user = login_type
 
-    current_user_decision = decision_tree
-    until current_user_decision == "Logout" 
-        # decision_tree
-        if current_user_decision == "Update your user account"
-            update_user_account(current_user)
-        elsif current_user_decision == "Delete your user account"
-            delete_user_account(current_user)
-            welcome_message
-            inputted_login_type = login_type
-        elsif current_user_decision == "Search for concerts"
-            search_for_concerts
-        elsif current_user_decision == "Buy ticket"
-            buy_ticket
-        elsif current_user_decision == "Sell ticket"
-            sell_ticket
+    current_user_decision = decision_tree(current_user)
+        until current_user_decision == "Logout"
+            decision_tree(current_user)
         end
-    end
-    logout
-    current_user = nil
-
 end
