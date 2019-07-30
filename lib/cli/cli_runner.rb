3a7f27776a9e77dda0dbcf57ab695a433571cf04
eliothowes1090 <ejhowes@gmail.com>
2019-07-30 19:21:29 +0100

    Created cli_runner and methods to ask user to login and then allows for basic CRUD functions. Needs to be refactored

--- lib/cli/cli_runner.rb
+++ lib/cli/cli_runner.rb
@@ -4,9 +4,10 @@ require_relative 'cli_welcome_and_login.rb'
 
 def cli_runner
     welcome_message
+
     inputted_login_type = login_type
     if inputted_login_type == "User Login"
-        user_login
+       current_user =  user_login
     elsif inputted_login_type == "Create new User Account"
         create_new_user_account
     elsif inputted_login_type == "Concert Login"
@@ -14,5 +15,25 @@ def cli_runner
     elsif inputted_login_type == "Create new Concert Account"
         create_new_organisation_account
     end
+
+    current_user_decision = decision_tree
+    until current_user_decision == "Logout" 
+        # decision_tree
+        if current_user_decision == "Update your user account"
+            update_user_account(current_user)
+        elsif current_user_decision == "Delete your user account"
+            delete_user_account(current_user)
+            welcome_message
+            inputted_login_type = login_type
+        elsif current_user_decision == "Search for concerts"
+            search_for_concerts
+        elsif current_user_decision == "Buy ticket"
+            buy_ticket
+        elsif current_user_decision == "Sell ticket"
+            sell_ticket
+        end
+    end
+    logout
+    current_user = nil
 
 end
